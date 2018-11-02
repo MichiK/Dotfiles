fe72ab3493c1c7732417694fa4aa03b891b6acdf
MichiK <6341204+der-michik@users.noreply.github.com>
2018-11-02 19:13:12 +0100

    Add redshift

---
+++ redshift/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+#
+# MichiK <michik@michik.net> wrote this file. As long as you retain this
+# notice you can do whatever you want with this stuff. If we meet some day,
+# and you think this stuff is worth it, you can buy me a beer in return.
+# ----------------------------------------------------------------------------
+
+set -e
+
+install_dotfile redshift.conf .config/redshift.conf
+
+# vim: set ts=2 sw=2 expandtab:
